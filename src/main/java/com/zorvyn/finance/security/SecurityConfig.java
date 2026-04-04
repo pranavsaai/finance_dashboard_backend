529f233527549c3faee9e39a0c1ed903836726b8
pranavsaai <pkuchipu2@gitam.in>
2026-04-04 15:27:50 +0530

    Refactor BCryptPasswordEncoder bean into dedicated AppConfig to avoid potential circular dependency and improve configuration separation

--- src/main/java/com/zorvyn/finance/security/SecurityConfig.java
+++ src/main/java/com/zorvyn/finance/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,10 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final AuthFilter authFilter;
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
