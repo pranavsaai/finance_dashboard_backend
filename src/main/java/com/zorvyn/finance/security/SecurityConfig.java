ac0e7e64a4f470c261db993c0833da8782b50a34
pranavsaai <pkuchipu2@gitam.in>
2026-04-04 20:12:11 +0530

    updated secuity config allowing only POST endpoint as public and rest are protected

--- src/main/java/com/zorvyn/finance/security/SecurityConfig.java
+++ src/main/java/com/zorvyn/finance/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.zorvyn.finance.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,7 +23,8 @@ public class SecurityConfig {
 
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()   
-                .requestMatchers("/api/users").permitAll()     
+                .requestMatchers("/api/users").permitAll()   
+                .requestMatchers(HttpMethod.POST, "/api/users").permitAll()  
                 .anyRequest().authenticated()                
             )
 
